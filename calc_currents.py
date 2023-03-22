f2548e45ac31bc72dd2a9ca7ec4169518524727c
hio2hio <zrf802@alumni.ku.dk>
2023-03-22 16:22:06 +0100

    run black

--- calc_currents.py
+++ calc_currents.py
@@ -18,35 +18,34 @@ Submitted 2018
 
 import argparse
 import os
-import sys
 import pickle
-from ase import Atoms
+import sys
 from ase.io import read
-from ase.io import write
 from ase.units import Hartree
 from gpaw import FermiDirac
 from gpaw import GPAW
 from gpaw.lcao.tools import dump_hamiltonian_parallel
 from gpaw.lcao.tools import get_bfi
 from numpy import ascontiguousarray as asc
-import gpaw
 import matplotlib
 import numpy as np
+
 # To retrieve path to utils_zcolor which should be placed in the same folder as
 # calc_current.py
 from pathlib import Path
+
 home = str(Path.home())
-sys.path.append(home + '/bin/py_scripts/calc_current')
+sys.path.append(home + "/bin/py_scripts/calc_current")
 import utils_zcolor
 from libgradient import Gradient
 
-matplotlib.use('agg')
+matplotlib.use("agg")
 
 
 def read_config(config_file: str) -> dict:
     with open(config_file) as file:
         lines = file.readlines()
-        values = [line.split('=') for line in lines]
+        values = [line.split("=") for line in lines]
         config_values = {}
         for value in values:
             config_values[value[0]] = value[1].strip()
@@ -55,22 +54,10 @@ def read_config(config_file: str) -> dict:
 
 
 def main() -> None:
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument(
-        '--path',
-        default='data/',
-        help='path to data folder'
-    )
-    parser.add_argument(
-        '--basis',
-        default='dzp',
-        help='basis (sz, dzp, ...)'
-    )
-    parser.add_argument(
-        '--config',
-        default=None,
-        help='name of the config file'
-    )
+    parser = argparse.ArgumentParser(description="Process some integers.")
+    parser.add_argument("--path", default="data/", help="path to data folder")
+    parser.add_argument("--basis", default="dzp", help="basis (sz, dzp, ...)")
+    parser.add_argument("--config", default=None, help="name of the config file")
     args = parser.parse_args()
     path = str(os.path.abspath(args.path)) + "/"
 
@@ -80,7 +67,7 @@ def main() -> None:
 
     # Load config values (e.g. h-spacing, functional, align atoms etc.)
     if config is None:
-        raise FileNotFoundError('No config file has been chosen')
+        raise FileNotFoundError("No config file has been chosen")
 
     config_values = read_config(config)
 
@@ -90,23 +77,23 @@ def main() -> None:
     else:
         ef = 0.0
 
-    if 'functional' in config_values:
-        xc = config_values['functional']
+    if "functional" in config_values:
+        xc = config_values["functional"]
 
         # To make sure the right basis set is used
         basis += "." + xc
     else:
-        xc = 'PBE'
+        xc = "PBE"
 
     h_basis = "sz." + xc
 
-    if 'h_spacing' in config_values:
-        h = float(config_values['h_spacing'])
+    if "h_spacing" in config_values:
+        h = float(config_values["h_spacing"])
     else:
         h = 0.2
 
-    if 'charge' in config_values:
-        charge = int(config_values['charge'])
+    if "charge" in config_values:
+        charge = int(config_values["charge"])
     else:
         charge = 0
 
@@ -118,8 +105,8 @@ def main() -> None:
     # Constants
     FDwidth: float = 0.1
     kpts = (1, 1, 1)
-    mode: str = 'lcao'
-    eV2au: float = 1/Hartree
+    mode: str = "lcao"
+    eV2au: float = 1 / Hartree
     bias: float = 1e-3 * eV2au
     gamma: float = 1e0 * eV2au
     ef: float = ef * eV2au  # If ef has been set to a custom value.
@@ -128,29 +115,29 @@ def main() -> None:
     energy_grid = np.arange(estart, eend, energy_step)
 
     # Align z-axis and cutoff at these atoms, OBS paa retningen.
-    if 'bottom_atom' not in config_values:
-        raise ValueError('You need to specify the bottom atom to align molecule')
+    if "bottom_atom" not in config_values:
+        raise ValueError("You need to specify the bottom atom to align molecule")
 
-    if 'top_atom' not in config_values:
-        raise ValueError('You need to specify the top atom to align molecule')
+    if "top_atom" not in config_values:
+        raise ValueError("You need to specify the top atom to align molecule")
 
-    align1: int = int(config_values['bottom_atom']) - 1
-    align2: int = int(config_values['top_atom']) - 1
+    align1: int = int(config_values["bottom_atom"]) - 1
+    align2: int = int(config_values["top_atom"]) - 1
 
     basis_full = {
-        'H': h_basis,
-        'C': basis,
-        'S': basis,
-        'N': basis,
-        'Si': basis,
-        'Ge': basis,
-        'B': basis,
-        'O': basis,
-        'F': basis,
-        'Cl': basis,
-        'P': basis,
-        'Br': basis,
-        'Ru': basis
+        "H": h_basis,
+        "C": basis,
+        "S": basis,
+        "N": basis,
+        "Si": basis,
+        "Ge": basis,
+        "B": basis,
+        "O": basis,
+        "F": basis,
+        "Cl": basis,
+        "P": basis,
+        "Br": basis,
+        "Ru": basis,
     }
 
     # Divider for h - size of the arrows for current density
@@ -177,15 +164,15 @@ def main() -> None:
         kpts=kpts,
         mode=mode,
         nbands="nao",
-        symmetry={'point_group': False, 'time_reversal': False},
+        symmetry={"point_group": False, "time_reversal": False},
         charge=charge,
-        txt="logfile.txt"
+        txt="logfile.txt",
     )
     atoms.set_calculator(calc)
     atoms.get_potential_energy()  # Converge everything!
-    print('Fermi is', atoms.calc.get_fermi_level())
+    print("Fermi is", atoms.calc.get_fermi_level())
 
-    dump_hamiltonian_parallel(path + 'scat_', atoms, direction='z')
+    dump_hamiltonian_parallel(path + "scat_", atoms, direction="z")
 
     # Write AO basis to disk
     bfs = get_bfi(calc, range(len(atoms)))
@@ -194,9 +181,11 @@ def main() -> None:
     phi_xg = calc.wfs.basis_functions.gd.zeros(len(c_fo_xi))
     gd0 = calc.wfs.gd
 
+    # THIS FUCKS UP WHEN RAN IN PARALLEL!
     calc.wfs.basis_functions.lcao_to_grid(c_fo_xi, phi_xg, -1)
+
     # Writing this to disk while going multiprocessing with GPAW
-    # Reason: Numpy tries to pickle the objects, but gd0 is 
+    # Reason: Numpy tries to pickle the objects, but gd0 is
     # not pickleable as it's a MPI object
     # np.save(path + "ao_basis_grid", [phi_xg, gd0])
     utils_zcolor.plot_basis(atoms, phi_xg, folder_name=path + "basis/ao")
@@ -242,54 +231,52 @@ def main() -> None:
     print("Calculating transmission - AO-basis")
 
     # To optimize the following matrix operations
-    Gamma_L = Gamma_L.astype(dtype='float32', order='F')
-    Gamma_R = Gamma_R.astype(dtype='float32', order='F')
-    Gr = Gr.astype(dtype='complex128', order='F')
+    Gamma_L = Gamma_L.astype(dtype="float32", order="F")
+    Gamma_R = Gamma_R.astype(dtype="float32", order="F")
+    Gr = Gr.astype(dtype="complex128", order="F")
     trans = utils_zcolor.calc_trans(energy_grid, Gr, Gamma_L, Gamma_R)
 
     utils_zcolor.plot_transmission(
-        energy_grid*Hartree, np.real(trans), path + plot_basename + "trans.png"
+        energy_grid * Hartree, np.real(trans), path + plot_basename + "trans.png"
     )
-    np.save(
-        path + data_basename + 'trans_full.npy', [energy_grid*Hartree, trans]
-    )
+    np.save(path + data_basename + "trans_full.npy", [energy_grid * Hartree, trans])
     print("AO-transmission done!")
 
     # Find HOMO and LUMO
     for n in range(len(eig_vals)):
         if eig_vals[n] < 0 and eig_vals[n + 1] > 0:
-            HOMO = eig_vals[n]
-            LUMO = eig_vals[n + 1]
-            midgap = (HOMO + LUMO)/2.0
+            homo = eig_vals[n]
+            lumo = eig_vals[n + 1]
+            midgap = (homo + lumo) / 2.0
 
             np.savetxt(
-                path + "basis/mo/" + 'homo_index.txt',
-                X=['HOMO index is ', n],
-                fmt='%.10s',
-                newline='\n',
+                path + "basis/mo/" + "homo_index.txt",
+                X=["HOMO index is ", n],
+                fmt="%.10s",
+                newline="\n",
             )
             break
 
     hl_gap = [
-        'HOMO is ',
-        HOMO*Hartree,
-        'LUMO is ',
-        LUMO*Hartree,
-        'mid-gap is ',
-        midgap*Hartree
+        "HOMO is ",
+        homo * Hartree,
+        "LUMO is ",
+        lumo * Hartree,
+        "mid-gap is ",
+        midgap * Hartree,
     ]
-    np.savetxt(path + 'HOMO_LUMO.txt', X=hl_gap, fmt='%.10s', newline='\n')
+    np.savetxt(path + "HOMO_LUMO.txt", X=hl_gap, fmt="%.10s", newline="\n")
 
-    """Current with fermi functions"""
+    # Current with fermi functions
     fR, fL = utils_zcolor.fermi_ongrid(energy_grid, ef, bias)
     dE = energy_grid[1] - energy_grid[0]
-    current_trans = (1 / (2*np.pi)) * np.array(
-        [trans[en].real * (fL[en]-fR[en])*dE for en in range(len(energy_grid))]
+    current_trans = (1 / (2 * np.pi)) * np.array(
+        [trans[en].real * (fL[en] - fR[en]) * dE for en in range(len(energy_grid))]
     ).sum()
     np.save(path + data_basename + "current_trans.npy", current_trans)
 
-    """Current approx at low temp"""
-    sigma_r = -1j/2. * (GamL + GamR)  # + V_pot
+    # Current approx at low temp
+    sigma_r = -1j / 2.0 * (GamL + GamR)  # + V_pot
     Gr_approx = utils_zcolor.retarded_gf2(H_ao, S_ao, ef, sigma_r)
 
     Gles = np.dot(np.dot(Gr_approx, GamL), Gr_approx.T.conj())
@@ -299,12 +286,12 @@ def main() -> None:
     utils_zcolor.plot_complex_matrix(Gles, path + "Gles")
 
     Tt = np.dot(np.dot(np.dot(GamL, Gr_approx), GamR), Gr_approx.T.conj())
-    current_dV = (bias/(2*np.pi))*Tt.trace()
+    current_dV = (bias / (2 * np.pi)) * Tt.trace()
     np.save(path + "data/trans_dV.npy", [ef, Tt.trace()])
     np.save(path + "data/current_dV.npy", current_dV)
 
     # Non corrected current - AO basis
-    mlt = 1j*Gles/(4*np.pi)
+    mlt = 1j * Gles / (4 * np.pi)
     np.save(path + "data/Gles_dV.npy", mlt)
 
     x_cor = gd0.coords(0)
@@ -322,7 +309,7 @@ def main() -> None:
     eig = eig.take(order)
     vec = vec.take(order, axis=1)
     S_mo = np.dot(np.dot(vec.T.conj(), S_ao), vec)
-    vec = vec/np.sqrt(np.diag(S_mo))
+    vec = vec / np.sqrt(np.diag(S_mo))
     S_mo = np.dot(np.dot(vec.T.conj(), S_ao), vec)
     H_mo = np.dot(np.dot(vec.T, H_ao), vec)
 
@@ -334,22 +321,26 @@ def main() -> None:
     Gamma_R_mo = np.swapaxes(Gamma_R_mo, 0, 2)
 
     # Convert and save mlt in MO basis
-    sigma_r_mo = -1j/2. * (GamL_mo + GamR_mo)  # + V_pot
+    sigma_r_mo = -1j / 2.0 * (GamL_mo + GamR_mo)  # + V_pot
     Gr_approx_mo = utils_zcolor.retarded_gf2(H_mo, S_mo, ef, sigma_r_mo)
 
     Gles_mo = np.dot(np.dot(Gr_approx_mo, GamL_mo), Gr_approx_mo.T.conj())
     Gles_mo *= bias
-    np.save(path + data_basename + "matrices_dV_mo.npy", [Gr_approx_mo, Gles_mo, GamL_mo])
+    np.save(
+        path + data_basename + "matrices_dV_mo.npy", [Gr_approx_mo, Gles_mo, GamL_mo]
+    )
 
     utils_zcolor.plot_complex_matrix(Gles_mo, path + "Gles_mo")
 
-    Tt_mo = np.dot(np.dot(np.dot(GamL_mo, Gr_approx_mo), GamR_mo), Gr_approx_mo.T.conj())
-    current_dV_mo = (bias/(2*np.pi))*Tt_mo.trace()  # Single spin current
+    Tt_mo = np.dot(
+        np.dot(np.dot(GamL_mo, Gr_approx_mo), GamR_mo), Gr_approx_mo.T.conj()
+    )
+    current_dV_mo = (bias / (2 * np.pi)) * Tt_mo.trace()  # Single spin current
     np.save(path + "data/trans_dV_mo.npy", [ef, Tt_mo.trace()])
     np.save(path + "data/current_dV_mo.npy", current_dV_mo)
 
     # Non corrected current - MO basis
-    mlt_mo = 1j*Gles_mo/(4*np.pi)
+    mlt_mo = 1j * Gles_mo / (4 * np.pi)
     np.save(path + "data/Gles_dV_mo.npy", mlt_mo)
 
     print("Calculating gradient..")
@@ -376,8 +367,8 @@ def main() -> None:
     jy_cut = jy[:, :, SI:EI]
     jz_cut = jz[:, :, SI:EI]
     print(f"jz_cut shape: {jz_cut.shape}")
-    cut_off = jz_cut.max()/cutoff
-    multiplier = 1/(2*np.sqrt(jx_cut**2 + jy_cut**2 + jz_cut**2).max())
+    cut_off = jz_cut.max() / cutoff
+    multiplier = 1 / (2 * np.sqrt(jx_cut**2 + jy_cut**2 + jz_cut**2).max())
 
     # Sixth last arg is the divider for the real space grid, multiplier gives a thicker diameter
     print("Writing .spt files..")
@@ -394,11 +385,11 @@ def main() -> None:
         cut_off,
         path,
         align1,
-        align2
+        align2,
     )
 
     utils_zcolor.plot_convergence()
 
 
-if __name__ == '__main__':
-    exit(main())
+if __name__ == "__main__":
+    sys.exit(main())
--- utils_zcolor.py
+++ utils_zcolor.py
@@ -1,9 +1,9 @@
-import numpy as np
 from ase import Atoms
-from ase.io import write
 from ase.io import read
+from ase.io import write
 from matplotlib.colors import LinearSegmentedColormap
 import matplotlib.pyplot as plt
+import numpy as np
 
 c = ["#007fff", "#ff3616"]  # Blue, Red
 
@@ -19,20 +19,18 @@ def get_eigenchannels(Gr_approx, GamL):
 
 
 def plot_eigenchannels(
-    atoms: Atoms,
-    teig_phi_xg,
-    folder_name: str = "./basis/eigenchannels"
+    atoms: Atoms, teig_phi_xg, folder_name: str = "./basis/eigenchannels"
 ) -> None:
     for idx, phi in enumerate(teig_phi_xg):
         write(f"{folder_name}/{idx}.cube", atoms, data=phi)
 
 
-def plot_basis(atoms, phi_xG, folder_name='./basis') -> None:
+def plot_basis(atoms, phi_xG, folder_name="./basis") -> None:
     """
     atoms: Atoms-object
     """
     for n, phi in enumerate(phi_xG):
-        write(f'{folder_name}/{n}.cube', atoms, data=phi)
+        write(f"{folder_name}/{n}.cube", atoms, data=phi)
 
 
 def identify_and_align(molecule, alignatom1: int, alignatom2: int):
@@ -51,8 +49,8 @@ def identify_and_align(molecule, alignatom1: int, alignatom2: int):
 
     molecule.center(vacuum=vacuum)
 
-    elec1 = Atoms('H', positions=[molecule[sI].position])
-    elec2 = Atoms('H', positions=[molecule[eI].position])
+    elec1 = Atoms("H", positions=[molecule[sI].position])
+    elec2 = Atoms("H", positions=[molecule[eI].position])
 
     del molecule[eI]
     del molecule[sI]
@@ -67,9 +65,9 @@ def identify_and_align(molecule, alignatom1: int, alignatom2: int):
 
 def plot_transmission(energy_grid, trans, save_name: str) -> None:
     plt.plot(energy_grid, trans)
-    plt.yscale('log')
-    plt.ylabel(r'Transmission')
-    plt.xlabel(r'E-E$_F$ (eV)')
+    plt.yscale("log")
+    plt.ylabel(r"Transmission")
+    plt.xlabel(r"E-E$_F$ (eV)")
     plt.savefig(save_name)
     plt.close()
 
@@ -81,7 +79,18 @@ def calc_trans(energy_grid, gret, gamma_left, gamma_right):
     Presumably because the matrices are pretty much the same and the overhead
     from calculating efficiency outweighs doing the calculation.
     """
-    trans = np.array([np.matmul(np.matmul(np.matmul(gamma_left[:, :, en], gret[:, :, en]), gamma_right[:, :, en]), gret[:, :, en].T.conj()).trace() for en in range(len(energy_grid))])
+    trans = np.array(
+        [
+            np.matmul(
+                np.matmul(
+                    np.matmul(gamma_left[:, :, en], gret[:, :, en]),
+                    gamma_right[:, :, en],
+                ),
+                gret[:, :, en].T.conj(),
+            ).trace()
+            for en in range(len(energy_grid))
+        ]
+    )
 
     return trans
 
@@ -90,11 +99,11 @@ def plot_complex_matrix(matrix, save_name: str) -> None:
     fig, axes = plt.subplots(nrows=1, ncols=2)
 
     for dat, axis in zip([matrix.real, matrix.imag], axes.flat):
-        image = axis.matshow(dat, cmap='seismic', vmin=dat.min(), vmax=dat.max())
-        cb = fig.colorbar(image, ax=axis, orientation='horizontal')
+        image = axis.matshow(dat, cmap="seismic", vmin=dat.min(), vmax=dat.max())
+        cb = fig.colorbar(image, ax=axis, orientation="horizontal")
         cb.formatter.set_powerlimits((0, 0))
         cb.update_ticks()
-        plt.cm.get_cmap('seismic')
+        plt.cm.get_cmap("seismic")
 
     plt.savefig(save_name)
     plt.close()
@@ -104,7 +113,7 @@ def retarded_gf(h_ao, s_ao, energy, gamma_left, gamma_right):
     """
     Retarded Gf using approx
     """
-    return np.linalg.inv(energy*s_ao - h_ao + (1j/2.)*(gamma_left + gamma_right))
+    return np.linalg.inv(energy * s_ao - h_ao + (1j / 2.0) * (gamma_left + gamma_right))
 
 
 def retarded_gf2(h_ao, s_ao, energy, sigma_ret):
@@ -113,7 +122,7 @@ def retarded_gf2(h_ao, s_ao, energy, sigma_ret):
     """
     eta = 1e-10
 
-    return np.linalg.inv(-sigma_ret + (energy + eta*1.j)*s_ao - h_ao)
+    return np.linalg.inv(-sigma_ret + (energy + eta * 1.0j) * s_ao - h_ao)
 
 
 def ret_gf_ongrid(energy_grid, h_ao, s_ao, gamma_left, gamma_right):
@@ -121,7 +130,12 @@ def ret_gf_ongrid(energy_grid, h_ao, s_ao, gamma_left, gamma_right):
     Put the retarded gf on an energy grid
     """
     ret_gf = np.array(
-        [retarded_gf(h_ao, s_ao, energy_grid[en], gamma_left[:, :, en], gamma_right[:, :, en]) for en in range(len(energy_grid))]
+        [
+            retarded_gf(
+                h_ao, s_ao, energy_grid[en], gamma_left[:, :, en], gamma_right[:, :, en]
+            )
+            for en in range(len(energy_grid))
+        ]
     )
     ret_gf = np.swapaxes(ret_gf, 0, 2)
 
@@ -136,7 +150,7 @@ def fermi(energy, mu_):
     # old = 1./(np.exp((energy - mu_)/kbt_) + 1.)
     kbt_ = 25e-6
 
-    return 0.5 + 0.5*np.tanh(-((energy - mu_)/kbt_)/2)
+    return 0.5 + 0.5 * np.tanh(-((energy - mu_) / kbt_) / 2)
 
 
 def fermi_ongrid(energy_grid, e_f, bias):
@@ -144,15 +158,15 @@ def fermi_ongrid(energy_grid, e_f, bias):
     f_right = []
 
     for en in energy_grid:
-        f_left.append(fermi(en, e_f + bias/2.))
-        f_right.append(fermi(en, e_f - bias/2.))
+        f_left.append(fermi(en, e_f + bias / 2.0))
+        f_right.append(fermi(en, e_f - bias / 2.0))
 
     return f_left, f_right
 
 
 def create_colorlist(colors):
     n_bins = [201]  # Discretizes the interpolation into bins
-    cmap_name = 'my_list'
+    cmap_name = "my_list"
     cm = LinearSegmentedColormap.from_list(cmap_name, colors, N=n_bins[0])
     colorlist = []
 
@@ -177,12 +191,16 @@ def plot_current(
     align1: int,
     align2: int,
 ) -> None:
-    refmol = read(path + 'central_region.xyz')
+    refmol = read(path + "central_region.xyz")
     refcoord1 = refmol[align1 + 1].position
     refcoord2 = refmol[align2 + 1].position
 
     # make colorlist for zcolor
-    colors = [[140/255., 0, 255/255.], [1, 1, 1], [255/255., 165/255., 0]]  # Y -> W -> P
+    colors = [
+        [140 / 255.0, 0, 255 / 255.0],
+        [1, 1, 1],
+        [255 / 255.0, 165 / 255.0, 0],
+    ]  # Y -> W -> P
     z_colorlist = create_colorlist(colors)
 
     # make colorlist for cylindrical color
@@ -190,9 +208,9 @@ def plot_current(
     cyl_colorlist = create_colorlist(colors)
 
     au2A = 0.529177249
-    x = x[::grid_size]*au2A
-    y = y[::grid_size]*au2A
-    z = z[::grid_size]*au2A
+    x = x[::grid_size] * au2A
+    y = y[::grid_size] * au2A
+    z = z[::grid_size] * au2A
 
     jz = jz[::grid_size, ::grid_size, ::grid_size]
     jy = jy[::grid_size, ::grid_size, ::grid_size]
@@ -216,51 +234,60 @@ def plot_current(
         for iy, y2 in enumerate(y):
             for iz, z2 in enumerate(z):
                 norm2 = np.sqrt(
-                    jx[ix, iy, iz]**2 + jy[ix, iy, iz]**2 + jz[ix, iy, iz]**2
+                    jx[ix, iy, iz] ** 2 + jy[ix, iy, iz] ** 2 + jz[ix, iy, iz] ** 2
                 )
-                norm = np.sqrt(jz[ix, iy, iz]**2)
+                norm = np.sqrt(jz[ix, iy, iz] ** 2)
 
                 if norm2 > cut_off and z2 > refcoord1[2] and z2 < refcoord2[2]:
-                    rel_z = jz[ix, iy, iz]/norm2
-                    z_color = z_colorlist[
-                        int(np.round(rel_z, decimals=2)*100) + 100
+                    rel_z = jz[ix, iy, iz] / norm2
+                    z_color = z_colorlist[int(np.round(rel_z, decimals=2) * 100) + 100]
+
+                    rel_phi = (
+                        jy[ix, iy, iz]
+                        * np.cos(np.arctan2(y2 - refcoord1[1], x2 - refcoord1[0]))
+                        - jx[ix, iy, iz]
+                        * np.sin(np.arctan2(y2 - refcoord1[1], x2 - refcoord1[0]))
+                    ) / norm2
+                    cyl_color = cyl_colorlist[
+                        int(np.round(rel_phi, decimals=2) * 100) + 100
                     ]
 
-                    rel_phi = (jy[ix, iy, iz]*np.cos(np.arctan2(y2 - refcoord1[1], x2 - refcoord1[0])) - jx[ix, iy, iz]*np.sin(np.arctan2(y2 - refcoord1[1], x2 - refcoord1[0])))/norm2
-                    cyl_color = cyl_colorlist[int(np.round(rel_phi, decimals=2)*100) + 100]
-
-                    z_list.append("draw arrow{0} arrow color {8} diameter {7} {{ {1},{2},{3} }} {{ {4},{5},{6} }} \n".format(
-                        a,
-                        x2 - jx[ix, iy, iz]/(size*norm2),
-                        y2 - jy[ix, iy, iz]/(size*norm2),
-                        z2 - jz[ix, iy, iz]/(size*norm2),
-                        x2 + jx[ix, iy, iz]/(size*norm2),
-                        y2 + jy[ix, iy, iz]/(size*norm2),
-                        z2 + jz[ix, iy, iz]/(size*norm2),
-                        norm*multiplier,
-                        z_color)
+                    z_list.append(
+                        "draw arrow{0} arrow color {8} diameter {7} {{ {1},{2},{3} }} {{ {4},{5},{6} }} \n".format(
+                            a,
+                            x2 - jx[ix, iy, iz] / (size * norm2),
+                            y2 - jy[ix, iy, iz] / (size * norm2),
+                            z2 - jz[ix, iy, iz] / (size * norm2),
+                            x2 + jx[ix, iy, iz] / (size * norm2),
+                            y2 + jy[ix, iy, iz] / (size * norm2),
+                            z2 + jz[ix, iy, iz] / (size * norm2),
+                            norm * multiplier,
+                            z_color,
+                        )
                     )
 
-                    cyl_list.append("draw arrow{0} arrow color {8} diameter {7} {{ {1},{2},{3} }} {{ {4},{5},{6} }} \n".format(
-                        a,
-                        x2 - jx[ix, iy, iz]/(size*norm2),
-                        y2 - jy[ix, iy, iz]/(size*norm2),
-                        z2 - jz[ix, iy, iz]/(size*norm2),
-                        x2 + jx[ix, iy, iz]/(size*norm2),
-                        y2 + jy[ix, iy, iz]/(size*norm2),
-                        z2 + jz[ix, iy, iz]/(size*norm2),
-                        norm*multiplier,
-                        cyl_color)
+                    cyl_list.append(
+                        "draw arrow{0} arrow color {8} diameter {7} {{ {1},{2},{3} }} {{ {4},{5},{6} }} \n".format(
+                            a,
+                            x2 - jx[ix, iy, iz] / (size * norm2),
+                            y2 - jy[ix, iy, iz] / (size * norm2),
+                            z2 - jz[ix, iy, iz] / (size * norm2),
+                            x2 + jx[ix, iy, iz] / (size * norm2),
+                            y2 + jy[ix, iy, iz] / (size * norm2),
+                            z2 + jz[ix, iy, iz] / (size * norm2),
+                            norm * multiplier,
+                            cyl_color,
+                        )
                     )
                 a += 1
 
     z_list.append("set defaultdrawarrowscale 0.1 \n")
-    z_list.append('rotate 90 \n')
-    z_list.append('background white \n')
+    z_list.append("rotate 90 \n")
+    z_list.append("background white \n")
 
     cyl_list.append("set defaultdrawarrowscale 0.1 \n")
-    cyl_list.append('rotate 90 \n')
-    cyl_list.append('background white \n')
+    cyl_list.append("rotate 90 \n")
+    cyl_list.append("background white \n")
 
     with open(savename + "_zcolor.spt", "w") as text_file:
         text_file.writelines(z_list)
@@ -270,15 +297,14 @@ def plot_current(
 
 
 def plot_convergence() -> None:
-    _, _, jz, x, y, z = np.load(
-        "./data/data/current_all.npy", allow_pickle=True
+    _, _, jz, x, y, z = np.load("./data/data/current_all.npy", allow_pickle=True)
+    current = (
+        np.abs(np.real(np.load("./data/data/current_dV_mo.npy", allow_pickle=True)))
+        * 6.623618183e-3
     )
-    current = np.abs(
-        np.real(np.load("./data/data/current_dV_mo.npy", allow_pickle=True))
-    )*6.623618183e-3
-    z *= 0.529177249*0.1
-    dA = (x[1] - x[0])*(y[1] - y[0])
-    jz_sum = np.abs(jz.sum(axis=(0, 1))*dA*6.623618183e-3)
+    z *= 0.529177249 * 0.1
+    dA = (x[1] - x[0]) * (y[1] - y[0])
+    jz_sum = np.abs(jz.sum(axis=(0, 1)) * dA * 6.623618183e-3)
 
     fig, ax = plt.subplots()
     ax.plot(z, jz_sum, c=c[0], label="xy-plane current")
@@ -287,6 +313,6 @@ def plot_convergence() -> None:
 
     ax.set_xlabel("z-coordinate (nm)")
     ax.set_ylabel("Current (A)")
-    
+
     plt.legend(frameon=False)
     plt.savefig("./data/convergence_plot.png", dpi=150)
